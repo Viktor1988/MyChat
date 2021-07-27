6ff80065a1271024ccc2fc92dc8df282831189fb
u_m0tzu <vitya.popov@gmail.com>
2021-07-27 14:28:19 +0300

    Initial Commit

---
+++ MyChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyChat
+//
+//  Created by Виктор Попов on 27.07.2021.
+//  Copyright © 2021 Виктор Попов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
